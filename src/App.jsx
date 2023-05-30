bb7deb34dce83d7f8ab3f0168ed9ee2e0d2dc50d
Ivarsky <iamh90@gmail.com>
2023-05-30 21:05:10 +0000

    More custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+
 import Players from './Players'
 import Teams from './Teams'
+import useToggle from '../useToggle'
 
 function App() {
+  const {value: showTeams, toggle} = useToggle(false)
   return (
       <div>
-        <Teams color='red'/>
+        <button onClick={toggle}>{ showTeams ? 'Hide Teams' : 'Show Teams'}</button>
+        { showTeams && <Teams color='red'/>}
         <Players backgroundColor='lightblue'/>
       </div>
   )
